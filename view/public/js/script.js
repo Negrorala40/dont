f66475a1ec29372727b27651198379100609ea7d
shernanb <valen751998@hotmail.com>
2024-10-02 16:09:52 -0500

    actualizado

---
+++ view/public/js/script.js
@@ -0,0 +1,45 @@
+// Muestra en la consola el valor devuelto por la siguiente operación
+console.log(
+    // Accede al elemento con el id "usuario" en el documento
+    document.getElementById("usuario")
+    
+    // Cambia el contenido de texto de ese elemento a "un nuevo usuario"
+    .textContent = "Usuario"
+);
+// Accede al documento HTML actual
+document
+
+// Busca el primer (y único) elemento que tiene el id "usuario"
+.getElementById("usuario")
+
+// Retorna el elemento HTML correspondiente si se encuentra, o null si no existe
+// Accede al documento HTML actual
+document
+
+// Busca todos los elementos que tienen la clase "boton"
+.getElementsByClassName("boton")
+
+// Retorna una colección de nodos (HTMLCollection) con todos los elementos que tienen esa clase
+// Accede al documento HTML actual
+document
+
+// Busca todos los elementos que tienen la etiqueta "p" (parrágrafos)
+.getElementsByTagName("p")
+
+// Retorna una colección de nodos (HTMLCollection) con todos los elementos que tienen esa etiqueta
+
+// Accede al documento HTML actual
+document
+
+// Selecciona el primer elemento que coincida con el selector CSS ".boton"
+.querySelector(".boton")
+
+// Retorna el primer elemento que coincide con el selector, o null si no encuentra ninguno
+
+// Accede al documento HTML actual
+document
+
+// Selecciona todos los elementos que coincidan con el selector CSS ".boton"
+.querySelectorAll(".boton")
+
+// Retorna una NodeList con todos los elementos que coinciden con el selector
